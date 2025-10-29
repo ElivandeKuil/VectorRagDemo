3fbf77204236874197e10e68637340e1e3f9d18b
Eli van de Kuil <elivandekuil@gmail.com>
2025-10-29 21:58:09 +0100

    Model naar models map

---
+++ Models/ChatRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace VectorRagDemo.Models
+{
+    public class ChatRequest
+    {
+        public string Query { get; set; } = string.Empty;
+        public List<ChatMessage>? History { get; set; }
+    }
+}
--- Views/Chat/ChatController.cs
+++ Views/Chat/ChatController.cs
@@ -24,10 +24,4 @@ namespace VectorRagDemo.Controllers
             return Json(await _chatService.Ask(request));
         }
     }
-
-    public class ChatRequest
-    {
-        public string Query { get; set; } = string.Empty;
-        public List<ChatMessage>? History { get; set; }
-    }
 }
